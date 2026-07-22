816c3af8448b4e7f8d9ee5b7881555c879e936f2
TheBigOx <ajbarry99@gmail.com>
2026-07-23 06:30:36 +1000

    Remove deprecated Docusaurus files and update blog content for clarity and precision

--- docs/.docusaurus/client-modules.js
+++
@@ -1,7 +0,0 @@
-export default [
-  require("/Users/anthonybarry/repos/playrunner/docs/.docusaurus/docusaurus-plugin-css-cascade-layers/default/layers.css"),
-  require("/Users/anthonybarry/repos/playrunner/docs/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/anthonybarry/repos/playrunner/docs/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/Users/anthonybarry/repos/playrunner/docs/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/Users/anthonybarry/repos/playrunner/docs/src/css/custom.css"),
-];
