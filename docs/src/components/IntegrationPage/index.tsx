c482a6404d57bdd6775c11dbc61fd94530adb803
TheBigOx <ajbarry99@gmail.com>
2026-07-28 05:06:33 +1000

    feat(integration): replace Webhooks icon with SVG and update styles

--- docs/src/components/IntegrationPage/index.tsx
+++ docs/src/components/IntegrationPage/index.tsx
@@ -7,7 +7,6 @@ import JiraLogo from '../../../../packages/jira/assets/jira.svg';
 import OpenAILogo from '../../../../packages/openai/assets/openai.svg';
 import PlaywrightLogo from '../../../../packages/playwright/assets/playwright.svg';
 import SlackLogo from '../../../../packages/slack/assets/slack.svg';
-import WebhooksLogo from '../../../../packages/webhooks/assets/webhooks.svg';
 import styles from './styles.module.css';
 
 type IconKind =
@@ -53,7 +52,17 @@ function renderIntegrationIcon(icon: IconKind) {
     case 'slack':
       return <SlackLogo aria-hidden="true" />;
     case 'webhooks':
-      return <WebhooksLogo aria-hidden="true" />;
+      return (
+        <svg
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+        >
+          <path d="M18 16.98h-5.99c-1.1 0-1.95.94-2.48 1.9A4 4 0 0 1 2 17c.01-.7.2-1.4.57-2" />
+          <path d="m6 17 3.13-5.78c.53-.97.1-2.18-.5-3.1a4 4 0 1 1 6.89-4.06" />
+          <path d="m12 6 3.13 5.73C15.66 12.7 16.9 13 18 13a4 4 0 0 1 0 8" />
+        </svg>
+      );
     case 'environment':
       return (
         <svg
